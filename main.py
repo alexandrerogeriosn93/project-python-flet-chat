eeec28f490d0f1ee5e712c2ce360a042aedb2be0
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-11-12 08:42:05 -0300

    feat: create main function

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello world")
+import flet as ft
+
+
+def main(page: ft.Page):
+    page.title = "Flet Chat"
+    page.horizontal_alignment = ft.CrossAxisAlignment.STRETCH
+
+
+ft.app(target=main)
